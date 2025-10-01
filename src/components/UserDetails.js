fa4af6b915411d17a8b7fc1168f97967300ee40c
leonaajd <ajdareviqleona@gmail.com>
2025-10-01 22:44:31 +0200

    user detail page

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,3 +1,21 @@
-export default function UserDetails() {
-    return <h2>User Details</h2>;
+import { useParams, Link } from "react-router-dom";
+
+export default function UserDetails({ users }) {
+  const { id } = useParams();
+  const user = users.find((user) => String(user.id) === id);
+
+  if (!user) return <p>User not found!</p>;
+  return (
+    <div style={{ padding: 10 }}>
+      <Link to="/">Back</Link>
+
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <p>
+        Address: {user.address?.city}, {user.address?.street}
+      </p>
+    </div>
+  );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,33 +1,32 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function UserList({users}) {
-    const [search, setSearch] = useState("");
+export default function UserList({ users }) {
+  const [search, setSearch] = useState("");
 
-    const filtered = users.filter(
-        (user) =>
-            user.name.toLowerCase().includes(search.toLowerCase()) ||
-            user.email.toLowerCase().includes(search.toLowerCase())
-    );
+  const filtered = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.toLowerCase())
+  );
 
-    return (
-        <div style={{ padding: 10}}>
-        <h2>Users</h2>
-        <input
+  return (
+    <div style={{ padding: 10 }}>
+      <h2>Users</h2>
+      <input
         placeholder="Search by name or email"
         value={search}
         onChange={(element) => setSearch(element.target.value)}
-        />
-        <ul>
-            {filtered.map((user) => (
-                <li key={user.id}>
-                    <Link to={`/user/${user.id}`}>
-                        {user.name} - {user.email} ({user.company?.name})
-                    </Link>
-                </li>
-            ))}
-        </ul>
-
-        </div>
-    );
+      />
+      <ul>
+        {filtered.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>
+              {user.name} - {user.email} ({user.company?.name})
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
