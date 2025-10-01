be6a0d5a32268c445bf4054a4115bb659f1b0fc5
leonaajd <ajdareviqleona@gmail.com>
2025-10-01 23:05:36 +0200

    add user form

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,3 +1,47 @@
-export default function AddUserForm() {
-    return <h2>Add User</h2>;
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function AddUserForm({ addUser }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!name || !email) {
+      setError("Name and Email are required");
+      return;
+    }
+    const newUser = {
+      id: Math.random(),
+      name,
+      email,
+      company: { name: "Link Plus" },
+    };
+    addUser(newUser);
+    navigate("/");
+  };
+
+  return (
+    <div style={{ padding: 10 }}>
+      <h2>Add User</h2>
+      {error && <p style={{ color: "red" }}>(error)</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <br />
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+    </div>
+  );
 }
