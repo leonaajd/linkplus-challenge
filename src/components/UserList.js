8a11649fc0f8d8b1592e59ccb2da090c09781e38
leonaajd <ajdareviqleona@gmail.com>
2025-10-01 22:26:23 +0200

    users list

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,3 +1,33 @@
-export default function UserList() {
-    return <h2>Users</h2>;
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function UserList({users}) {
+    const [search, setSearch] = useState("");
+
+    const filtered = users.filter(
+        (user) =>
+            user.name.toLowerCase().includes(search.toLowerCase()) ||
+            user.email.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+        <div style={{ padding: 10}}>
+        <h2>Users</h2>
+        <input
+        placeholder="Search by name or email"
+        value={search}
+        onChange={(element) => setSearch(element.target.value)}
+        />
+        <ul>
+            {filtered.map((user) => (
+                <li key={user.id}>
+                    <Link to={`/user/${user.id}`}>
+                        {user.name} - {user.email} ({user.company?.name})
+                    </Link>
+                </li>
+            ))}
+        </ul>
+
+        </div>
+    );
 }
