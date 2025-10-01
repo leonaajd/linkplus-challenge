1556feac6f89380716f2da5543bec9372f5361ab
leonaajd <ajdareviqleona@gmail.com>
2025-10-01 22:09:47 +0200

    fetching api, added addUser method to add new users

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import UserList from "./components/UserList";
 import UserDetails from "./components/UserDetails";
 import AddUserForm from "./components/AddUserForm";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const addUser = (newUser) => {
+    setUsers([newUser, ...users]);
+  };
+
   return (
     <BrowserRouter>
       <nav style={{ display: "flex", gap: 12, padding: 12 }}>
-        <Link to="/">Users</Link>
-        <Link to="/add">Add</Link>
-
+        <Link to="/">Users</Link> | <Link to="/add">Add</Link>
       </nav>
 
     <Routes>
-      <Route path="/" element={<UserList />} />
-      <Route path="/user/:id" element={<UserDetails />} />
-      <Route path="/add" element={<AddUserForm />} />
+      <Route path="/" element={<UserList users={users} />} />
+      <Route path="/user/:id" element={<UserDetails users={users} />} />
+      <Route path="/add" element={<AddUserForm addUser={addUser} />} />
     </Routes>
     
     </BrowserRouter>
